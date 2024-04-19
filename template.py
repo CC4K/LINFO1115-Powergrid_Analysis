6780a6ce078d218aa9721f8dcbc96c9886b9137f
Cédric Kheirallah <cedric.kheirallah@student.uclouvain.be>
2024-04-19 15:48:45 +0200

    adjusted all graphs + more details

--- template.py
+++ template.py
@@ -1,4 +1,6 @@
 import sys
+
+import matplotlib.pyplot as plt
 import pandas as pd
 from template_utils import *
 
@@ -8,7 +10,7 @@ sys.setrecursionlimit(6000)
 # Undirected graph
 # Task 1: Average degree, number of bridges, number of local bridges
 def Q1(dataframe):
-    # ------------ 1.1 ------------#
+    # 1.1
     # Average degree = Sum adjacency / Nbr nodes
 
     # create set for adjacency
@@ -25,25 +27,25 @@ def Q1(dataframe):
     # average degree
     avg_degree = sum_adjacency / n_nodes
 
+    #-------------------------------------#
     # plot => https://mathinsight.org/degree_distribution
     n_adjacency = [len(set_adjacency[node]) for node in set_adjacency]
-    plt.figure(1)
+    plt.figure(figsize=(8, 6))
     plt.hist(n_adjacency)
+    plt.grid(axis='y')
     plt.title("Degree of distribution")
     plt.xlabel("Degree")
-    plt.ylabel("Fraction of nodes")
+    plt.ylabel("Number of nodes")
     # plt.savefig("degree_of_distribution.png")
-    # plt.show()
-    ###############################
+    #-------------------------------------#
 
-    # ------------ 1.2 ------------#
+    # 1.2
     nbr_bridges = find_bridges(dataframe)
     # print("nbr de bridge:", nbr_bridges)
-    ###############################
 
-    # ------------ 1.3 ------------#
+    # 1.3
     # TODO
-    ###############################
+
     return [avg_degree, nbr_bridges, 0]  # [average degree, nb bridges, nb local bridges]
 
 
@@ -61,22 +63,24 @@ def Q2(dataframe):
         # calculate similarity score for each pair of node
         similarities.append(similarity(set_adjacency, A, B))
 
-    # plot percentage
+    #-------------------------------------#
+    # plot
     # turn list of scores into cumulative one : https://stackoverflow.com/questions/15889131/how-to-find-the-cumulative-sum-of-numbers-in-a-list
     cumulative_sum = np.cumsum(sorted(similarities))
     cumulative_percentage = (cumulative_sum / sum(similarities)) * 100
     # print(sorted(similarities))
     # print(cumulative_percentage)
 
-    plt.figure(2)
+    plt.figure(figsize=(8, 6))
     plt.plot(sorted(similarities), cumulative_percentage)  # percentage of edges vs similarity score
+    plt.xlim(0)
+    plt.ylim((0, 100))
+    plt.grid()
     plt.title("Cumulative graph of the percentage of edges vs their similarity score")
     plt.xlabel("Similarity score")
     plt.ylabel("Cumulative percentage of edges")
     # plt.savefig("cumulative_graph_of_similarity_score.png")
-    # plt.show()
-
-    ###############################
+    #-------------------------------------#
     # compute average similarity score for the network
     return np.sum(similarities) / len(similarities)
 
@@ -111,25 +115,31 @@ def Q4(dataframe):
     for big_dict in shortest_paths.values():
         for values in big_dict.values(): path_lengths.append(values)
 
-    # get diameter
+    # return number of each length of shortest paths
+    ret = [path_lengths.count(length) for length in range(np.max(path_lengths)+1)]
+
+
+    # diameter and most common shortest distance
     diameter = np.max(path_lengths)
     # print(diameter) # 46
-
-    # return number of each length of shortest paths
-    ret = [path_lengths.count(length) for length in range(diameter+1)]
+    commonest_dist = ret.index(np.max(ret))
+    # print(commonest_dist) # 19
 
     #-------------------------------------#
     # plot => LINFO1115-course1.pdf, pg 34
-    plt.figure(3)
+    plt.figure(figsize=(8, 6))
     x = np.arange(0, diameter+1)
-    plt.scatter(x, ret)
-    plt.plot(x, ret)
+    plt.plot(x, ret, '-bo', markersize=5)
+    plt.plot(commonest_dist, ret[commonest_dist], 'go', markersize=5, label="Most common shortest distance")
+    plt.plot(diameter, ret[diameter], 'ro', markersize=5, label="Longest shortest distance")
+    plt.xlim(0)
     plt.ylim(0)
+    plt.legend()
+    plt.grid()
     plt.title("Graph of the number of paths having a given distance")
     plt.xlabel("Number of intermediaries")
-    plt.ylabel("Number of chains")
+    plt.ylabel("Number of paths")
     # plt.savefig("small-world_phenomenon.png")
-    # plt.show()
     #-------------------------------------#
     return ret  # ret[0] = nbr shortest paths of length 0, ret[1] = nbr shortest paths of length 1, ...
     # Note that we will ignore the value at index 0 as it can be set to 0 or the number of nodes in the graph
@@ -138,7 +148,7 @@ def Q4(dataframe):
 # Undirected graph
 # Task 5: Betweenness centrality
 def Q5(dataframe):
-    # Your code here
+    # TODO
     return [0, 0.0]  # the id of the node with the highest betweenness centrality, the associated betweenness centrality value.
 
 
@@ -146,9 +156,10 @@ def Q5(dataframe):
 
 df = pd.read_csv('powergrid.csv')
 # df = pd.read_csv('testgrid.csv')
-# draw_graph(df)
 print("Q1", Q1(df))
 print("Q2", Q2(df))
 print("Q3", Q3(df))
 print("Q4", Q4(df))
 print("Q5", Q5(df))
+# draw_graph(df)
+# plt.show()
